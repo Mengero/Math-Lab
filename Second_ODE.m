c4d3f006cd15ed582b138346980606ab60be7f66
Mengero <30794820+Mengero@users.noreply.github.com>
2019-05-17 15:09:11 +0800

    Update Second_ODE.m

--- Second_ODE.m
+++ Second_ODE.m
@@ -11,3 +11,48 @@ legend('y_1','y_2')
 Yexact = Yexact(:,1);
 Exact_Sol=vpa([Xe Yexact],5)
 %%
+syms x y;
+%% Step size
+h=10^-4;
+%% Range of x
+xspan=[0,1];
+steps = (xspan(2)-xspan(1))/h;
+%% Initial values
+x = zeros(steps,1);
+y1 = zeros(steps,1);
+y2 = zeros(steps,1);
+y1(1)=1;x(1)=0;y2(1)=1;
+%% Define Equation Set
+%%
+f= @(x,y1,y2)y1*(1-y1)*y2+y1;
+g = @(x,y1,y2)y2;
+%% Euler's Method
+%%
+for j=2:steps+1
+    
+    x(j,1)=x(j-1,1)+h;
+    
+    y2(j,1)=y2(j-1,1)+h*f(x(j-1,1),y1(j-1,1),y2(j-1,1));
+    
+    y1(j,1)=y1(j-1,1)+h*g(x(j-1,1,y1(j-1,1),y2(j-1,1)));
+    
+end
+euler=vpa([x y],5)
+%% Heun's Method
+%%
+for j=2:steps+1
+    x(j,1)=x(j-1,1)+h;
+    
+    k1(j-1,1)=h*f(x(j-1,1),y1(j-1,1),y2(j-1,1));
+    
+    k2(j-1,1)=h*f(x(j-1,1),y1(j-1,1),y2(j-1,1));
+    
+    y2(j,1)=y2(j-1)+0.5*(k1(j-1)+k2(j-1));
+    
+    p1(j-1,1)=h*g(x(j-1,1),y1(j-1,1),y2(j-1,1));
+    
+    p2(j-1,1)=h*g(x(j-1,1),y1(j-1,1),y2(j-1,1));
+    
+    y1(j,1)=y1(j-1)+0.5*(p1(j-1)+p2(j-1));
+end
+heuns=vpa([x y1 y2],5)
